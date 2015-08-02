8af63e11f96b5263c08448e13a84598786802eaa
itger <z_piotr@yahoo.com>
2015-08-02 22:41:46 +0200

    first  commit

---
+++ app/src/main/java/pl/itger/dualsimcard/DualSIM_adapter.java
@@ -0,0 +1,42 @@
+package pl.itger.dualsimcard;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by piotrz on 7/23/15.
+ * Copyright 2015 Piotr Zerynger ITger
+ */
+
+class DualSIM_adapter extends ArrayAdapter<Sci> {
+
+   public DualSIM_adapter(Context context, ArrayList<Sci> items) {
+        super(context, R.layout.simci, items);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Sci telInf = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.simci, parent, false);
+        }
+        // Lookup view for data population
+        TextView tvName = (TextView) convertView.findViewById(R.id.SIMfirstLine);
+        TextView tvHome = (TextView) convertView.findViewById(R.id.SIMsecondLine);
+        // Populate the data into the template view using the data object
+        tvName.setText(telInf.s1);
+        tvHome.setText(telInf.s2);
+        // Return the completed view to render on screen
+        return convertView;
+        //return super.getView(position, convertView, parent);
+    }
+}
---
+++ app/src/main/java/pl/itger/dualsimcard/Sci.java
@@ -0,0 +1,17 @@
+package pl.itger.dualsimcard;
+
+/**
+ * Created by piotrz on 7/23/15.
+ * Copyright 2015 Piotr Zerynger ITger
+ */
+
+public class Sci {
+    String s1;
+    String s2;
+
+
+    public Sci(String s1, String s2) {
+        this.s1 = s1;
+        this.s2 = s2;
+    }
+}
